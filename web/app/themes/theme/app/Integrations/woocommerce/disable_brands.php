54f120eafdf6ea883bcf8bf5e7d89276080ea303
Ryan Allen <ryansallen53@gmail.com>
2026-02-22 18:20:04 +1000

    Add WooCommerce integration files for disabling brands and downloads

---
+++ web/app/themes/theme/app/Integrations/woocommerce/disable_brands.php
@@ -0,0 +1,7 @@
+<?php
+
+// namespace App\Integrations\WooCommerce;
+
+// add_action( 'init', function() {
+//     update_option( 'wc_feature_woocommerce_brands_enabled', 'no' );
+// } );
---
+++ web/app/themes/theme/app/Integrations/woocommerce/disable_downloads.php
@@ -0,0 +1,14 @@
+<?php
+
+// namespace App\Integrations\WooCommerce;
+
+// use WC_Product;
+
+// add_filter('woocommerce_is_downloadable', function ($is_downloadable, WC_Product $product) {
+//     return false;
+// }, 10, 2);
+
+// add_filter('woocommerce_account_menu_items', function ($items) {
+//     unset($items['downloads']);
+//     return $items;
+// });
